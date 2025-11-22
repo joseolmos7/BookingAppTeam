16c16c2d6e7ae00743d29a9e0b601370542d622e
joseo <Joseolmos7.jo@gmail.com>
2025-11-21 23:21:11 -0500

    Update: login and dashboard layouts and activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 }
 
 android {
+    buildFeatures{
+        viewBinding = true
+    }
     namespace = "com.example.bookingappteam"
     compileSdk = 36
 
---
+++ app/src/main/java/com/example/bookingappteam/DashboardActivity.kt
@@ -0,0 +1,40 @@
+package com.example.bookingappteam
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+import android.content.Intent
+import com.example.bookingappteam.databinding.ActivityDashboardBinding
+
+class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cardAvailableRooms.setOnClickListener {
+            val intent = Intent(this, AvailableRoomsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cardMyBookings.setOnClickListener {
+            val intent = Intent(this, MyBookingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cardSearchRooms.setOnClickListener {
+            val intent = Intent(this, SearchRoomsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cardBookRoom.setOnClickListener { }
+        val intent = Intent(this, BookRoomActivity::class.java)
+        startActivity(intent)
+    }
+}
